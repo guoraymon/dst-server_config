950efc0bdf39612465f055023d82cbd207de13a3
raymon <raymon.guo@outlook.com>
2020-04-19 19:27:22 +0800

    init volcano

---
+++ Master/modoverrides.lua
@@ -0,0 +1,250 @@
+return {
+  -- 中文语言包
+  ["workshop-367546858"]={
+    ["configuration_options"]={ ["LANG"]="auto", ["SMALL_TEXTURES"]=true },
+    ["enabled"]=true 
+  },
+  
+  -- 汉化增强
+  ["workshop-1418746242"]={
+    ["configuration_options"]={
+      ["clearfont"]=true,
+      ["eventplus"]=true,
+      ["extratrans"]=true,
+      ["workshop-345692228"]=0,
+      ["workshop-351325790"]=0,
+      ["workshop-352373173"]=0,
+      ["workshop-376333686"]=0,
+      ["workshop-378160973"]=0 
+    },
+    ["enabled"]=true 
+  },
+
+  -- Tropical Experience | The Volcano Biome
+  ["workshop-1505270912"]={
+    ["configuration_options"]={
+      [""]=0,
+      ["Hamlet"]=10,
+      ["Moon"]=10,
+      ["Shipwrecked"]=25,
+      ["Shipwrecked_plus"]=true,
+      ["Together"]=20,
+      ["Volcano"]=true,
+      ["Waves"]=true,
+      ["anthill"]=1,
+      ["aporkalypse"]=true,
+      ["betaversion"]=0,
+      ["boatlefthud"]=0,
+      ["city_tab"]=0,
+      ["disable_snow_effectst"]=false,
+      ["enableallprefabs"]=false,
+      ["flood"]=true,
+      ["frost_island"]=10,
+      ["hail"]=true,
+      ["hamlet_world"]=false,
+      ["home_tab"]=0,
+      ["interior_decorations"]=true,
+      ["kraken"]=1,
+      ["lilypad"]=2,
+      ["lobbyexit"]=false,
+      ["mactuskonice"]=2,
+      ["mangrove"]=3,
+      ["megarandomCompatibilityWater"]=false,
+      ["messagebottle"]=20,
+      ["musselfarm"]=300,
+      ["nautical_tab"]=true,
+      ["obsidian_tab"]=0,
+      ["octopusking"]=1,
+      ["pigcity1"]=15,
+      ["pigcity2"]=20,
+      ["pigruins"]=1,
+      ["rawling"]=1,
+      ["removedark"]=false,
+      ["seacucumber"]=100,
+      ["sealnado"]=true,
+      ["seataro"]=100,
+      ["seaweed"]=300,
+      ["set_idioma"]="stringsCh",
+      ["startlocation"]=5,
+      ["tar_pool"]=10,
+      ["tropicalshards"]=0,
+      ["watercontents"]=true,
+      ["waterygrave"]=10,
+      ["wind"]=true,
+      ["wreck"]=20 
+    },
+    ["enabled"]=true 
+  },
+
+  -- Tropical Experience Return of Them Complement
+  ["workshop-1754423272"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- Tropical Experience Return of Them Complement 2
+  ["workshop-1754437018"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- Tropical Experience | Lamp Post without alloy
+  ["workshop-2052669851"]={
+    ["configuration_options"]={ ["icon"]=0, ["ison"]=0, ["radius"]=5, ["recipe"]=1 },
+    ["enabled"]=true 
+  },
+
+  -- 能力勋章
+  ["workshop-1909182187"]={
+    ["configuration_options"]={
+      ["containerdrag_switch"]=false,
+      ["devourfunc_switch"]=true,
+      ["difficulty_switch"]=false,
+      ["language_switch"]="ch",
+      ["medal_goodmax_resources_multiple"]=0,
+      ["medal_goodmid_resources_multiple"]=1,
+      ["medalequipslots_switch"]=true,
+      ["medalpage_switch"]=false,
+      ["pick_switch"]=true,
+      ["transplant_switch"]=true 
+    },
+    ["enabled"]=true 
+  },
+
+  -- 花样风滚草
+  ["workshop-1944492666"]={
+    ["configuration_options"]={
+      ["advanced_equipments_multiple"]=0,
+      ["advanced_foods_multiple"]=0,
+      ["advanced_monsters_multiple"]=0,
+      ["advanced_resources_multiple"]=0,
+      ["advanced_tools_multiple"]=0,
+      ["basic_equipments_multiple"]=0,
+      ["basic_foods_multiple"]=1,
+      ["basic_monsters_multiple"]=1,
+      ["basic_resources_multiple"]=1,
+      ["basic_tools_multiple"]=0,
+      ["blueprints_multiple"]=1,
+      ["cancatch_tumbleweed_switch"]=true,
+      ["game_level"]=1,
+      ["language_switch"]="ch",
+      ["lucky_compensate"]=0,
+      ["luckypage_switch"]=false,
+      ["othermod_resource_switch"]=true,
+      ["preparedfoods_multiple"]=0,
+      ["preparedfoods_spice_multiple"]=0.05,
+      ["preparedfoods_warly_multiple"]=0,
+      ["resource_regenerating_switch"]=false,
+      ["revise_rockfruit_chance_switch"]=true,
+      ["scarce_equipments_multiple"]=0,
+      ["scarce_monsters_multiple"]=0,
+      ["scarce_resources_multiple"]=0,
+      ["scarce_tools_multiple"]=0,
+      ["season_weight_multiple"]=2,
+      ["seek_death_multiple"]=0,
+      ["special_welfare_multiple"]=0 
+    },
+    ["enabled"]=true 
+  },
+
+  
+
+  -- Show Me
+  ["workshop-666155465"]={
+    ["configuration_options"]={
+      ["chestB"]=-1,
+      ["chestG"]=-1,
+      ["chestR"]=-1,
+      ["food_estimation"]=-1,
+      ["food_order"]=0,
+      ["food_style"]=0,
+      ["lang"]="auto",
+      ["show_food_units"]=-1,
+      ["show_uses"]=-1 
+    },
+    ["enabled"]=true 
+  },
+
+  -- 简易血条
+  ["workshop-1207269058"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- 快速旅行
+  ["workshop-1530801499"]={
+    ["configuration_options"]={ ["Hunger_Cost"]=1, ["Ownership"]=false, ["Sanity_Cost"]=1 },
+    ["enabled"]=true 
+  },
+
+  -- 智能小木牌
+  ["workshop-1595631294"]={
+    ["configuration_options"]={
+      ["BundleItems"]=false,
+      ["Digornot"]=false,
+      ["DragonflyChest"]=false,
+      ["Icebox"]=false,
+      ["SaltBox"]=false 
+    },
+    ["enabled"]=true 
+  },
+
+  -- 全球定位
+  ["workshop-378160973"]={
+    ["configuration_options"]={
+      ["ENABLEPINGS"]=true,
+      ["FIREOPTIONS"]=2,
+      ["OVERRIDEMODE"]=false,
+      ["SHAREMINIMAPPROGRESS"]=true,
+      ["SHOWFIREICONS"]=true,
+      ["SHOWPLAYERICONS"]=true,
+      ["SHOWPLAYERSOPTIONS"]=2 
+    },
+    ["enabled"]=true 
+  },
+
+  -- 稻花助手
+  ["workshop-2064265201"]={
+    ["configuration_options"]={
+      ["drop_stack"]=10,
+      ["no_disease"]=false,
+      ["no_grassgekko"]=false,
+      ["quick_harvest"]=false,
+      ["quick_pick"]=false,
+      ["shortcut_drop"]=true,
+      ["stack_size"]=99 
+    },
+    ["enabled"]=true 
+  },
+
+  -- AssetManager
+  ["workshop-1944472866"]={
+    ["configuration_options"]={ ["assetdebug"]=false, ["assetload"]=false, ["assetmod"]=false, gc=false },
+    ["enabled"]=true 
+  },
+
+
+
+  -- 击杀宣告
+  ["workshop-1894295075"]={ ["configuration_options"]={ ["is_fish_announce"]=true }, ["enabled"]=true },
+
+  -- 彩色虫洞
+  ["workshop-362175979"]={ ["configuration_options"]={ ["Draw over FoW"]="disabled" }, ["enabled"]=true },
+
+  -- 火堆复活
+  ["workshop-569043634"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+
+
+  -- 二本垃圾桶
+  ["workshop-1214740157"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- 无限帐篷
+  ["workshop-356930882"]={
+    ["configuration_options"]={ ["uses"]=10000000, ["uses2"]=10000000 },
+    ["enabled"]=true 
+  },
+
+  -- 装备槽
+  ["workshop-375850593"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- 暖石耐久
+  ["workshop-466732225"]={ ["configuration_options"]={  }, ["enabled"]=true },
+
+  -- 死亡不掉落
+  ["workshop-661253977"]={
+    ["configuration_options"]={ ["amudiao"]=true, ["baodiao"]=1, ["kong"]=0, ["rendiao"]=2, ["zbdiao"]=true },
+    ["enabled"]=true 
+  },
+}
--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -1,11 +1,41 @@
 --There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
 
 --ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
-	--The Workshop id can be found at the end of the url to the mod's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
-	--ServerModSetup("350811795")
+        --The Workshop id can be found at the end of the url to the mod's Workshop page.
+        --Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+        --ServerModSetup("350811795")
 
 --ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
-	--The Workshop id can be found at the end of the url to the collection's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
-	--ServerModCollectionSetup("379114180")
+        --The Workshop id can be found at the end of the url to the collection's Workshop page.
+        --Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+        --ServerModCollectionSetup("379114180")
+
+ServerModSetup("367546858") -- 中文语言包
+ServerModSetup("1418746242") -- 汉化增强
+
+ServerModSetup("1505270912") -- Tropical Experience | The Volcano Biome
+ServerModSetup("1754423272") -- Tropical Experience Return of Them Complement
+ServerModSetup("1754437018") -- Tropical Experience Return of Them Complement 2
+ServerModSetup("2052669851") -- Tropical Experience | Lamp Post without alloy
+
+ServerModSetup("1909182187") -- 能力勋章
+ServerModSetup("1944492666") -- 花样风滚草
+
+ServerModSetup("666155465") -- Show Me
+ServerModSetup("1207269058") -- 简易血条
+ServerModSetup("1530801499") -- 快速旅行
+ServerModSetup("1595631294") -- 智能小木牌
+ServerModSetup("378160973") -- 全球定位
+
+ServerModSetup("2064265201") -- 稻花助手
+ServerModSetup("1944472866") -- AssetManager
+
+ServerModSetup("1894295075") -- 击杀宣告
+ServerModSetup("362175979") -- 彩色虫洞
+ServerModSetup("569043634") -- 火堆复活
+
+ServerModSetup("1214740157") -- 二本垃圾桶
+ServerModSetup("356930882") -- 无限帐篷
+ServerModSetup("375850593") -- 装备槽
+ServerModSetup("466732225") -- 暖石耐久
+ServerModSetup("661253977") -- 死亡不掉落
